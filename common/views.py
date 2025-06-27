7572ebe177ee970d58cf68bc4a948885ebea4db3
stefoto85 <you@example.com>
2025-06-27 13:07:20 +0300

    rigister-profile

--- common/views.py
+++ common/views.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 
 class HomePageView(TemplateView):
-    template_name = 'profiles/edit_profile.html'
+    template_name = 'profiles/edit-profile.html'
 
 
 # class ContactPageView(TemplateView):
