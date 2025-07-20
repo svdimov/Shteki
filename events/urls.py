3adac51f15f66a968aa1557b240a571c23535b7b
stefoto85 <you@example.com>
2025-07-21 01:16:08 +0300

    Rest:searchbar

--- events/urls.py
+++ events/urls.py
@@ -6,7 +6,7 @@ from events.api_views import PastEventsAPI
 urlpatterns = [
     path('new-events/', views.NewEventView.as_view(), name='new-events'),
     path('past-events/', views.PastEventView.as_view(), name='past-events'),
-    # path('api/past-events/', PastEventsAPI.as_view(), name='api-past-events'),
+    path('api/past-events/', PastEventsAPI.as_view(), name='api-past-events'),
 
     path('event/create/', views.CreateEventView.as_view(), name='create-event'),
     path('events/<int:pk>/', include([
