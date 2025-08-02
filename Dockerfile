03e013cd2b0d0571e0c6d7ff4f5b5a3710676260
stefoto85 <you@example.com>
2025-08-02 19:23:48 +0300

    nginx-gunicorn

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.12-slim
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+# Add this block BEFORE pip install if you use Pillow, psycopg2, or anything needing compilation!
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    gcc \
+    libpq-dev \
+    python3-dev \
+    libjpeg-dev \
+    zlib1g-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+RUN python manage.py collectstatic --noinput
+
+CMD ["gunicorn", "Shteki.wsgi:application", "--bind", "0.0.0.0:8000", "--workers=4"]
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -7,7 +9,20 @@ from accounts.models import Profile
 UserModel = get_user_model()
 
 
+# @receiver(post_save, sender=UserModel)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 @receiver(post_save, sender=UserModel)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile(sender: UserModel, instance: UserModel, created: bool, **kwargs: dict) -> None:
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(
+            user=instance,
+
+        )
+        send_mail(
+            subject='Welcome to Petstagram',
+            message='Thank you for registering with Petstagram! We are excited to have you on board.',
+            from_email=settings.COMPANY_EMAIL,  # Use default email settings
+            recipient_list=[instance.email],
+        )
