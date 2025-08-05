d60c97cf87df846e72981ef37d2a46337071eb79
stefoto85 <you@example.com>
2025-08-05 17:23:36 +0300

    admin-panel-reset

--- common/admin.py
+++ common/admin.py
@@ -13,20 +13,4 @@ from common.models import EventParticipation
 
 @admin.register(EventParticipation)
 class EventParticipationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'event', 'status')
-    actions = ['set_status_will_go', 'set_status_maybe', 'set_status_not_going']
-
-    def set_status_will_go(self, request, queryset):
-        updated = queryset.update(status=StatusChoice.WILL_GO)
-        self.message_user(request, f"{updated} участници бяха маркирани като 'Will go'.")
-    set_status_will_go.short_description = "Set status to 'Will go'"
-
-    def set_status_maybe(self, request, queryset):
-        updated = queryset.update(status=StatusChoice.MAYBE)
-        self.message_user(request, f"{updated} участници бяха маркирани като 'Maybe'.")
-    set_status_maybe.short_description = "Set status to 'Maybe'"
-
-    def set_status_not_going(self, request, queryset):
-        updated = queryset.update(status=StatusChoice.NOT_GOING)
-        self.message_user(request, f"{updated} участници бяха маркирани като 'Not going'.")
-    set_status_not_going.short_description = "Set status to 'Not going'"
+    pass
